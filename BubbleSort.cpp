a006139c5489ea87b29d1eb3b8351f2f7fc41b60
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2024-11-01 01:23:42 +0600

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -21,10 +21,12 @@ void Bubble_Sort(int n,int arr[])
 }
 int main()
 {
+    /*
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
+    */
     int n;cin>>n;
     int arr[n];
     for(int i=0;i<n;i++)
