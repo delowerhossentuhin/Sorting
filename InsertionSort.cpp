a3aacea3571a97e2d07b76b10fd5211eb930bdbe
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2024-11-01 01:24:03 +0600

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -23,10 +23,12 @@ void Insertion_Sort(int n,int arr[])
 }
 int main()
 {
+    /*
     #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
+    */
     int n;cin>>n;
     int arr[n];
     for(int i=0;i<n;i++)
