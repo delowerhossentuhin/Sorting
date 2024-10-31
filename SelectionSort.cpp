37a95e65cbbacf0f99776016d14f02cb008b9f2a
D.H. Tuhin <128525189+delowerhossentuhin@users.noreply.github.com>
2024-11-01 01:24:31 +0600

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -34,10 +34,12 @@ void Print_Array(int arr[],int size)
 }
 int main()
 {
-     #ifndef ONLINE_JUDGE
+     /*
+    #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
     freopen("output.txt","w",stdout);
     #endif
+    */
     int n;cin>>n;
     int arr[n];
     for(int i=0;i<n;i++)
